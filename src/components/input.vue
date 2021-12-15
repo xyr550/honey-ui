c86e191e2fc6ff81266c8655a302e44c08951514
“wangying” <2973745148@qq.com>
2021-12-15 15:52:49 +0800

    ✨ 特性: input组件

---
+++ src/components/input.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="ho-input">
+    <input
+      class="ho-input_inner"
+      :type="type"
+      :placeholder="placeholder"
+      :name="name"
+      :disabled="disabled">
+  </div>
+</template>
+
+<script>
+import {
+  defineComponent
+} from 'vue'
+
+export default defineComponent({
+  name: 'HoInput',
+  props: {
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    type: {
+      type: String,
+      default: 'text'
+    },
+    name: {
+      type: String,
+      default: ''
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup() {
+
+  }
+});
+</script>
+
+<style lang="less" scoped>
+.ho-input {
+  width: 100%;
+  position: relative;
+  font-size: 14px;
+  display: inline-block;
+  .ho-input_inner {
+    -webkit-appearance: none;
+    background: #fff;
+    background-image: none;
+    border-radius: 4px;
+    border: 1px solid #dcdfe6;
+    box-sizing: border-box;
+    color: #606266;
+    display: inline-block;
+    font-size: inherit;
+    height: 40px;
+    line-height: 40px;
+    outline: none;
+    padding: 0 15px;
+    transition: border-color .2s cubic-bezier(.645,.045,.355,1);
+    width: 100%;
+    &:focus{
+      outline: none;
+      border-color: #409eff;
+    }
+  }
+}
+</style>
